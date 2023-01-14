97b61c29da97156c5ef9e9eeec2b02f6ea77ee68
Andreas Salzmann <andi.salzmann@googlemail.com>
2023-01-14 11:22:09 +0100

    added calculation for cell voltage, supply voltage and cell temperature

--- msp430_workspace/ircom/main.c
+++ msp430_workspace/ircom/main.c
@@ -1,111 +1,149 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2012, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//  MSP430FR59xx Demo - eUSCI_A0 UART echo at 9600 baud using BRCLK = 8MHz
-//
-//  Description: This demo echoes back characters received via a PC serial port.
-//  SMCLK/ DCO is used as a clock source and the device is put in LPM3
-//  The auto-clock enable feature is used by the eUSCI and SMCLK is turned off
-//  when the UART is idle and turned on when a receive edge is detected.
-//  Note that level shifter hardware is needed to shift between RS232 and MSP
-//  voltage levels.
-//
-//  The example code shows proper initialization of registers
-//  and interrupts to receive and transmit data.
-//  To test code in LPM3, disconnect the debugger.
-//
-//  ACLK = VLO, MCLK =  DCO = SMCLK = 8MHz
-//
-//                MSP430FR5969
-//             -----------------
-//       RST -|     P2.0/UCA1TXD|----> PC (echo)
-//            |                 |
-//            |                 |
-//            |     P2.1/UCA1RXD|<---- PC
-//            |                 |
-//
-//   P. Thanigai
-//   Texas Instruments Inc.
-//   August 2012
-//   Built with IAR Embedded Workbench V5.40 & Code Composer Studio V5.5
-//******************************************************************************
-// 
-//
-// Edit:  changed to IrDA PHY and basic hello world string output on P2.5 and P2.6
-//        VS Code resources: http://minkbot.blogspot.com/2019/03/vscode-and-msp430-debugging.html, https://github.com/michkrom/msp430-ansluta/tree/master/.vscode
-#include "msp430.h"
-#include <string.h>
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+
+#include <msp430.h>
 #include <stdint.h>
+#include <math.h>
+
+
+
+// ====== GPIO OUTPUT CHANNELS ====== // 
+
+#define NTC_SUP_PIN BIT0
+#define NTC_SUP_DIR P3DIR
+#define NTC_SUP_OUT P3OUT
+
+#define EN_U_MEAS_PIN BIT5
+#define EN_U_MEAS_DIR P3DIR
+#define EN_U_MEAS_OUT P3OUT
+
+#define EN_IR_SMD_PIN BIT7
+#define EN_IR_SMD_DIR P3DIR
+#define EN_IR_SMD_OUT P3OUT
+
+
+// ====== ANALOG CHANNELS ====== // 
+
+#define NTC_SUP_MEAS_PIN BIT2
+#define NTC_SUP_MEAS_DIR P1DIR
+#define NTC_SUP_MEAS_SEL0 P1SEL0
+#define NTC_SUP_MEAS_SEL1 P1SEL1
+#define NTC_SUP_MEAS_AINCH ADC12INCH_2
+
+#define NTC_U_MEAS_PIN BIT1
+#define NTC_U_MEAS_DIR P1DIR
+#define NTC_U_MEAS_SEL0 P1SEL0
+#define NTC_U_MEAS_SEL1 P1SEL1
+#define NTC_U_MEAS_AINCH ADC12INCH_1
+
+#define U_MEAS_PIN BIT0
+#define U_MEAS_DIR P1DIR
+#define U_MEAS_SEL0 P1SEL0
+#define U_MEAS_SEL1 P1SEL1
+#define U_MEAS_AINCH ADC12INCH_0
+
+
+// ====== IRDA CHANNELS ====== //
+
+#define IRDA_RX_PIN BIT6
+#define IRDA_TX_PIN BIT5
+#define IRDA_DIR P2DIR
+#define IRDA_SEL0 P2SEL0
+#define IRDA_SEL1 P2SEL1
+
+
 
 
 const char* str = "Hello World\n";
+volatile uint16_t adc_ntc_u, adc_ntc_sup, adc_batt_u, adc_vcc_u;
+
+double internalSupplyVoltage = 0;
+double BattCellVoltage = 0;
+double NTCSupplyVoltage = 0;
+double BattNTCTemperature = 0;
+double NTCVoltage = 0;
+double NTC_R = 0;
 
 
-/**********Global Variables****************/
 
+int main() {
 
-
-
-int main(void)
-{
   WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
 
   // Configure GPIO
-  P2SEL1 |= BIT5 | BIT6;                    // USCI_A0 UART operation
-  P2SEL0 &= ~(BIT5 | BIT6);
+  
+  P1DIR = 0;
+  P2DIR = 0;
+  P3DIR = 0;
+  P4DIR = 0;
+  PJDIR = 0;
+
+  P1OUT = 0;
+  P2OUT = 0;
+  P3OUT = 0;
+  P4OUT = 0;
+  PJOUT = 0;
+
+
+  // setup all enable pins
+
+  NTC_SUP_DIR |= NTC_SUP_PIN;
+  NTC_SUP_OUT |= NTC_SUP_PIN;
+
+  EN_U_MEAS_DIR |= EN_U_MEAS_PIN;
+  EN_U_MEAS_OUT |= EN_U_MEAS_PIN;
+
+  EN_IR_SMD_DIR |= EN_IR_SMD_PIN;
+  EN_IR_SMD_OUT |= EN_IR_SMD_PIN;
+
+
+
 
   // Disable the GPIO power-on default high-impedance mode to activate
-  // previously configured port settings
-  PM5CTL0 &= ~LOCKLPM5;
+    // previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;
 
-  // Startup clock system with max DCO setting ~8MHz
-  CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
-  CSCTL1 = DCOFSEL_3 | DCORSEL;             // Set DCO to 8MHz
-  CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-  CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
-  CSCTL0_H = 0;                             // Lock CS registers
+    // Startup clock system with max DCO setting ~8MHz
+    CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
+    CSCTL1 = DCOFSEL_3 | DCORSEL;             // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
+    CSCTL0_H = 0;                             // Lock CS registers
+
+
+  // setup ADC 
+  // By default, REFMSTR=1 => REFCTL is used to configure the internal reference
+  while(REFCTL0 & REFGENBUSY);              // If ref generator busy, WAIT
+  REFCTL0 |= REFVSEL_2 | REFON;             // Select internal ref = 2.5V
+                                            // Internal Reference ON
+
+  // Configure ADC12
+  ADC12CTL0 = ADC12SHT0_2 | ADC12ON;
+  ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;                     // ADCCLK = MODOSC; sampling timer, do sequence of channels
+  ADC12CTL2 |= ADC12RES_2;                  // 12-bit conversion results
+  ADC12CTL3 |= ADC12BATMAP;                 // select battery monitor
+  ADC12IER0 |= ADC12IE3;                    // Enable ADC conv complete interrupt
+  ADC12MCTL0 = ADC12INCH_31 | ADC12VRSEL_0; // VBAT ADC input select; Vref= VCC
+  ADC12MCTL1 = U_MEAS_AINCH | ADC12VRSEL_0; // A0: Voltage of battery
+  ADC12MCTL2 = NTC_SUP_MEAS_AINCH | ADC12VRSEL_0; // A1: Voltage of NTC Supply
+  ADC12MCTL3 = NTC_U_MEAS_AINCH | ADC12VRSEL_0 | ADC12EOS;   // A2: Voltage on NTC, end of sequence
+
+  while(!(REFCTL0 & REFGENRDY));            // Wait for reference generator
+                                            // to settle
+
+
+  // setup IRDA Pins
+
+  P2SEL1 |= IRDA_RX_PIN | IRDA_TX_PIN;                    // USCI_A0 UART operation
+  P2SEL0 &= ~(IRDA_RX_PIN | IRDA_TX_PIN);
+
+
+
+
+
 
   // Configure USCI_A0 for UART mode
   UCA1CTLW0 = UCSWRST;                      // Put eUSCI in reset
@@ -132,19 +170,41 @@ int main(void)
   UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
   // UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt || not needed in hello world
 
-  
+
   uint8_t i = 0;
 
-  while (1) 
+  while (1)
   {
     
+
+     // calc internal supply voltage µC
+      internalSupplyVoltage = adc_vcc_u / 4096.0 * 2.8 * 2;              // 2.8 = Vref = VCC
+
+     // calc battery cell voltage
+      BattCellVoltage = adc_batt_u / 4096.0 * 2.8 / 1.44 + 2.2;          // 1.44 = opv gain 2.2 = z-diode
+
+      // calc battery cell temperature
+      NTCSupplyVoltage = adc_ntc_sup / 4096.0 * 2.8;                                    // check ntc supply voltage with 2.8 V reference
+      NTCVoltage = adc_ntc_u/4096.0 * 2.8;                                              // check ntc voltage with 2.8 V reference
+      NTC_R = (-NTCVoltage * 1000.0) / (NTCVoltage - NTCSupplyVoltage);                 // calc NTC resistent
+      BattNTCTemperature = 1 / ((1/3977.0) * log(NTC_R/2700.0) + (1/298.0)) -273.15;    // calc temperature
+
+
+    // send string to UART interface
+
     for(i = 0; i < strlen(str); i++){
       while(UCA1STATW & UCBUSY);
       UCA1TXBUF = str[i];
     }
 
+    // start ADC conversion
+
+    ADC12CTL0 |= ADC12ENC | ADC12SC;         // Sampling and conversion start
+
+
     __delay_cycles(8e6);
 
+    __bis_SR_register(GIE);
   }
 
 
@@ -168,12 +228,73 @@ void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
       // insert received data
       // irPHY.put_received_data(UCA1RXBUF);
       break;
-    case USCI_UART_UCTXIFG: 
+    case USCI_UART_UCTXIFG:
       // the last char was successfully transferred
       // send next data
       // irPHY.send_next_data();
       break;
     case USCI_UART_UCSTTIFG: break;
     case USCI_UART_UCTXCPTIFG: break;
+  }
+}
+
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch (__even_in_range(ADC12IV, ADC12IV_ADC12RDYIFG))
+  {
+    case ADC12IV_NONE:        break;        // Vector  0:  No interrupt
+    case ADC12IV_ADC12OVIFG:  break;        // Vector  2:  ADC12MEMx Overflow
+    case ADC12IV_ADC12TOVIFG: break;        // Vector  4:  Conversion time overflow
+    case ADC12IV_ADC12HIIFG:  break;        // Vector  6:  ADC12BHI
+    case ADC12IV_ADC12LOIFG:  break;        // Vector  8:  ADC12BLO
+    case ADC12IV_ADC12INIFG:  break;        // Vector 10:  ADC12BIN
+    case ADC12IV_ADC12IFG0:   break;        // Vector 12:  ADC12MEM0 Interrupt
+    case ADC12IV_ADC12IFG1:   break;        // Vector 14:  ADC12MEM1
+    case ADC12IV_ADC12IFG2:   break;        // Vector 16:  ADC12MEM2
+    case ADC12IV_ADC12IFG3:           // Vector 18:  ADC12MEM3
+      adc_vcc_u = ADC12MEM0;
+      adc_batt_u = ADC12MEM1;
+      adc_ntc_sup = ADC12MEM2;
+      adc_ntc_u = ADC12MEM3;
+      // __bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
+      break;        
+    case ADC12IV_ADC12IFG4:   break;        // Vector 20:  ADC12MEM4
+    case ADC12IV_ADC12IFG5:   break;        // Vector 22:  ADC12MEM5
+    case ADC12IV_ADC12IFG6:   break;        // Vector 24:  ADC12MEM6
+    case ADC12IV_ADC12IFG7:   break;        // Vector 26:  ADC12MEM7
+    case ADC12IV_ADC12IFG8:   break;        // Vector 28:  ADC12MEM8
+    case ADC12IV_ADC12IFG9:   break;        // Vector 30:  ADC12MEM9
+    case ADC12IV_ADC12IFG10:  break;        // Vector 32:  ADC12MEM10
+    case ADC12IV_ADC12IFG11:  break;        // Vector 34:  ADC12MEM11
+    case ADC12IV_ADC12IFG12:  break;        // Vector 36:  ADC12MEM12
+    case ADC12IV_ADC12IFG13:  break;        // Vector 38:  ADC12MEM13
+    case ADC12IV_ADC12IFG14:  break;        // Vector 40:  ADC12MEM14
+    case ADC12IV_ADC12IFG15:  break;        // Vector 42:  ADC12MEM15
+    case ADC12IV_ADC12IFG16:  break;        // Vector 44:  ADC12MEM16
+    case ADC12IV_ADC12IFG17:  break;        // Vector 46:  ADC12MEM17
+    case ADC12IV_ADC12IFG18:  break;        // Vector 48:  ADC12MEM18
+    case ADC12IV_ADC12IFG19:  break;        // Vector 50:  ADC12MEM19
+    case ADC12IV_ADC12IFG20:  break;        // Vector 52:  ADC12MEM20
+    case ADC12IV_ADC12IFG21:  break;        // Vector 54:  ADC12MEM21
+    case ADC12IV_ADC12IFG22:  break;        // Vector 56:  ADC12MEM22
+    case ADC12IV_ADC12IFG23:  break;        // Vector 58:  ADC12MEM23
+    case ADC12IV_ADC12IFG24:  break;        // Vector 60:  ADC12MEM24
+    case ADC12IV_ADC12IFG25:  break;        // Vector 62:  ADC12MEM25
+    case ADC12IV_ADC12IFG26:  break;        // Vector 64:  ADC12MEM26
+    case ADC12IV_ADC12IFG27:  break;        // Vector 66:  ADC12MEM27
+    case ADC12IV_ADC12IFG28:  break;        // Vector 68:  ADC12MEM28
+    case ADC12IV_ADC12IFG29:  break;        // Vector 70:  ADC12MEM29
+    case ADC12IV_ADC12IFG30:  break;        // Vector 72:  ADC12MEM30
+    case ADC12IV_ADC12IFG31:  break;        // Vector 74:  ADC12MEM31
+    case ADC12IV_ADC12RDYIFG: break;        // Vector 76:  ADC12RDY
+    default: break;
   }
 }
